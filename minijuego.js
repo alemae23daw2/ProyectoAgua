09e0e102ceb917b92ab3cec055db84a679f9dc1c
alemae23daw2 <144940938+alemae23daw2@users.noreply.github.com>
2024-01-15 18:29:44 +0100

    sisissiisisis

--- minijuego.js
+++ minijuego.js
@@ -1,21 +1,52 @@
 window.onload = () => {
 
+    let timer = document.querySelector(".timer");
+    let minuts = 0;
+    let segons = 0;
+
+    mostrarTimer();
+
+    let timerInterval = setInterval(actualitzarTimer, 1000);
+
+    function actualitzarTimer() {
+        if (segons === 0) {
+            if (minuts > 0) {
+                minuts--;
+                segons = 59;
+            }
+        } else if(segons == 1){
+            segons--;
+            treureAigua();
+        }else{
+            segons--;
+        }
+        mostrarTimer();
+    }
+
+    function treureAigua(){
+        document.querySelector(".medidor").firstChild.remove();
+    }
+
+    function mostrarTimer() {
+        timer.innerHTML = `${minuts.toString().padStart(2, '0')}:${segons.toString().padStart(2, '0')}`;
+    }
+
     let ultimaPosicio;
     var recipiente = document.querySelectorAll('.recipiente');
-    [].forEach.call(recipiente, function(rec) {
+    [].forEach.call(recipiente, function (rec) {
         rec.addEventListener('dragstart', gestionarIniciDrag, false);
     });
-    [].forEach.call(recipiente, function(rec){
+    [].forEach.call(recipiente, function (rec) {
         rec.addEventListener('dragover', gestionarSobreDrag, false);
         rec.addEventListener('drop', gestionarDrop, false);
     });
 
     var llenado = document.querySelectorAll('.llenado');
-    [].forEach.call(llenado, function(lle){
+    [].forEach.call(llenado, function (lle) {
         lle.addEventListener('dragover', gestionarSobreDrag, false);
         lle.addEventListener('drop', gestionarDrop, false);
     });
-    [].forEach.call(llenado, function(lle) {
+    [].forEach.call(llenado, function (lle) {
         lle.addEventListener('dragstart', gestionarIniciDrag, false);
     });
 
@@ -33,16 +64,29 @@ window.onload = () => {
         var data = ev.dataTransfer.getData("img");
         ev.target.appendChild(document.getElementById(data).cloneNode(true));
         ultimaPosicio.firstElementChild.remove();
-        if(ev.target == recipiente[0]){
+        if (ev.target == recipiente[0]) {
             omplirAigua();
+            girar();
         }
     }
 
-    function omplirAigua(){
+    function omplirAigua() {
         let container = document.querySelector(".medidor");
         let aigua = document.createElement("div");
-        aigua.className = "aigua";
-        container.appendChild(aigua);
+        if (container.children.length <= 11) {
+            aigua.className = "aigua";
+            container.appendChild(aigua);
+            minuts += 1;
+        }
     }
-    
+
+    function girar(){
+        document.getElementById("bidon").style.transform = "rotate(-60deg)";
+        document.getElementById("bidon").parentElement.style.border = "1px solid transparent";
+        setTimeout(() => {
+            document.getElementById("bidon").style.transform = "rotate(0deg)";
+            document.getElementById("bidon").parentElement.style.border = "1px solid black";
+        }, 500);
+        
+    }
 }
