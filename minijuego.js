58f4fefd235fcb97f3e19bf92b5ff1d18c8a0870
alemae23daw2 <144940938+alemae23daw2@users.noreply.github.com>
2024-01-21 12:48:19 +0100

    POO

--- minijuego.js
+++ minijuego.js
@@ -1,98 +1,101 @@
 window.onload = () => {
 
-    let timer = document.querySelector(".timer");
-    let minuts = 0;
-    let segons = 0;
-
-    mostrarTimer();
-
-    let timerInterval = setInterval(actualitzarTimer, 1000);
-
-    function actualitzarTimer() {
-        if (segons === 0) {
-            if (minuts > 0) {
-                minuts--;
-                segons = 59;
+    class Minijuego {
+        constructor() {
+            this.timer = document.querySelector(".timer");
+            this.minutes = 0;
+            this.seconds = 0;
+            this.lastPosition = null;
+            this.timerInterval = setInterval(() => this.actualitzarTimer(), 1000);
+            this.listeners();
+            this.mostrarTimer();
+        }
+    
+        actualitzarTimer() {
+            if (this.seconds === 0) {
+                if (this.minutes > 0) {
+                    this.minutes--;
+                    this.seconds = 59;
+                }
+            } else if (this.seconds === 1) {
+                this.seconds--;
+                this.treureAigua();
+            } else {
+                this.seconds--;
             }
-        } else if(segons == 1){
-            segons--;
-            treureAigua();
-        }else{
-            segons--;
+            this.mostrarTimer();
         }
-        mostrarTimer();
-    }
-    function treureAigua(){
-        document.querySelector(".medidor").firstChild.remove();
-    }
-
-    function mostrarTimer() {
-        timer.innerHTML = `${minuts.toString().padStart(2, '0')}:${segons.toString().padStart(2, '0')}`;
-    }
-
-    let ultimaPosicio;
-    var recipiente = document.querySelectorAll('.recipiente');
-    [].forEach.call(recipiente, function (rec) {
-        rec.addEventListener('dragstart', gestionarIniciDrag, false);
-    });
-    [].forEach.call(recipiente, function (rec) {
-        rec.addEventListener('dragover', gestionarSobreDrag, false);
-        rec.addEventListener('drop', gestionarDrop, false);
-    });
-
-    var llenado = document.querySelectorAll('.llenado');
-    [].forEach.call(llenado, function (lle) {
-        lle.addEventListener('dragover', gestionarSobreDrag, false);
-        lle.addEventListener('drop', gestionarDrop, false);
-    });
-    [].forEach.call(llenado, function (lle) {
-        lle.addEventListener('dragstart', gestionarIniciDrag, false);
-    });
-
-    function gestionarSobreDrag(ev) {
-        ev.preventDefault();
-    }
-
-    function gestionarIniciDrag(ev) {
-        ultimaPosicio = document.getElementById("bidon").parentElement;
-        ev.dataTransfer.setData("img", ev.target.id);
-    }
-
-    function gestionarDrop(ev) {
-        ev.preventDefault();
-        var data = ev.dataTransfer.getData("img");
-        ev.target.appendChild(document.getElementById(data).cloneNode(true));
-        ultimaPosicio.firstElementChild.remove();
-        if (ev.target == recipiente[0]){
-            omplirAigua();
-            girar();
+    
+        treureAigua() {
+            document.querySelector(".medidor").firstChild.remove();
         }
-        if (ev.target == llenado[0]){
-            let aguaCau = document.getElementById("agua");
-            aguaCau.style.visibility = "visible";
-            setTimeout(()=>{
-                aguaCau.style.visibility="hidden";
+    
+        mostrarTimer() {
+            this.timer.innerHTML = `${this.minutes.toString().padStart(2, '0')}:${this.seconds.toString().padStart(2, '0')}`;
+        }
+    
+        listeners() {
+            let containers = document.querySelectorAll('.recipiente, .llenado');
+            containers.forEach(container => {
+                container.addEventListener('dragstart', ev => this.gestionarIniciDrag(ev), false);
+                container.addEventListener('dragover', ev => this.gestionarSobreDrag(ev), false);
+                container.addEventListener('drop', ev => this.gestionarDrop(ev), false);
+            });
+        }
+    
+    
+        gestionarSobreDrag(ev) {
+            ev.preventDefault();
+        }
+    
+        gestionarIniciDrag(ev) {
+            this.lastPosition = document.getElementById("bidon").parentElement;
+            ev.dataTransfer.setData("img", ev.target.id);
+        }
+    
+        gestionarDrop(ev) {
+            ev.preventDefault();
+            var data = ev.dataTransfer.getData("img");
+            ev.target.appendChild(document.getElementById(data).cloneNode(true));
+            this.lastPosition.firstElementChild.remove();
+            if (ev.target.classList.contains('recipiente')) {
+                this.omplirAigua();
+                this.girar();
+            }
+            if (ev.target.classList.contains('llenado')) {
+                this.mostrarAigua();
+            }
+        }
+    
+        omplirAigua() {
+            let container = document.querySelector(".medidor");
+            let aigua = document.createElement("div");
+            if (container.children.length <= 11) {
+                aigua.className = "aigua";
+                container.appendChild(aigua);
+                this.minutes += 1;
+            }
+        }
+    
+        girar() {
+            let bidon = document.getElementById("bidon");
+            bidon.style.transform = "rotate(-60deg)";
+            bidon.parentElement.style.border = "1px solid transparent";
+            setTimeout(() => {
+                bidon.style.transform = "rotate(0deg)";
+                bidon.parentElement.style.border = "1px solid black";
+            }, 500);
+        }
+    
+        mostrarAigua() {
+            let aiguaCau = document.getElementById("agua");
+            aiguaCau.style.visibility = "visible";
+            setTimeout(() => {
+                aiguaCau.style.visibility = "hidden";
             }, 1000);
         }
     }
+    
+    const game = new Minijuego();
 
-    function omplirAigua() {
-        let container = document.querySelector(".medidor");
-        let aigua = document.createElement("div");
-        if (container.children.length <= 11) {
-            aigua.className = "aigua";
-            container.appendChild(aigua);
-            minuts += 1;
-        }
-    }
-
-    function girar(){
-        document.getElementById("bidon").style.transform = "rotate(-60deg)";
-        document.getElementById("bidon").parentElement.style.border = "1px solid transparent";
-        setTimeout(() => {
-            document.getElementById("bidon").style.transform = "rotate(0deg)";
-            document.getElementById("bidon").parentElement.style.border = "1px solid black";
-        }, 500);
-        
-    }
 }
